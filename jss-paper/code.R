8c2dfca027a46f4d29f6aba54654ffaa16fbeeca
Bjarke Hautop <bjarke.kristensen@sund.ku.dk>
2026-06-22 16:17:16 +0200

    Update jss code

--- jss-paper/code.R
+++ jss-paper/code.R
@@ -2,12 +2,12 @@ library(causalDisco)
 
 ########## 3.1. Method objects
 data("tpc_example")
-pc_method <- pc(
+pc_pcalg <- pc(
   engine = "pcalg",
   test = "fisher_z",
   alpha = 0.05
 )
-fit <- disco(data = tpc_example, method = pc_method)
+fit <- disco(data = tpc_example, method = pc_pcalg)
 
 
 ######### 3.2. Background knowledge representation
@@ -20,8 +20,11 @@ kn <- knowledge(
     2 ~ C + D
   )
 )
+kn
+
 
 plot(kn)
+
 
 kn2 <- knowledge(
   tpc_example,
@@ -31,28 +34,35 @@ kn2 <- knowledge(
     3 ~ starts_with("oldage")
   )
 )
+kn2
 
-fit <- disco(data = tpc_example, method = pc_method, knowledge = kn2)
+tpc_cd <- tpc(
+  engine = "causalDisco",
+  test = "fisher_z",
+  alpha = 0.05
+)
+fit2 <- disco(data = tpc_example, method = tpc_cd, knowledge = kn2)
 
 ######### 4 Data analysis example
 data("nlsy97")
+nlsy97_cc <- nlsy97[complete.cases(nlsy97), ]
 pc_pcalg <- pc(engine = "pcalg", test = "conditional_gaussian", alpha = 0.05)
-result_pcalg <- disco(nlsy97, pc_pcalg)
+result_pcalg <- disco(nlsy97_cc, pc_pcalg)
 plot(result_pcalg)
 
 
 # Requires having set up Tetrad to run. See package README for instructions.
 
 pc_tetrad <- pc(engine = "tetrad", test = "conditional_gaussian", alpha = 0.05)
-result_pcalg <- disco(nlsy97, pc_pcalg)
+result_pcalg <- disco(nlsy97_cc, pc_pcalg)
 
 plot(result_pcalg)
 
 pc_tetrad <- pc(engine = "tetrad", test = "conditional_gaussian", alpha = 0.05)
-disco(nlsy97, pc_tetrad)
+disco(nlsy97_cc, pc_tetrad)
 
 ges_pcalg <- ges(
   engine = "pcalg",
   score = "bic_cg"
 )
-disco(nlsy97, ges_pcalg)
+disco(nlsy97_cc, ges_pcalg)
